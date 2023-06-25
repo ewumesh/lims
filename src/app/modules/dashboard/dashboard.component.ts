0c1e2eb3aaaa4c1ea309d3460a1ffc2ac0186a07
umeshthapa12 <umeshthapa112233@gmail.com>
2023-06-25 18:41:39 +0545

    update

--- src/app/modules/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/dashboard.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { BaseChartDirective } from 'ng2-charts';
 import { DashboarService } from 'src/app/services/dashboard/dashboard.service';
-import DatalabelsPlugin from 'chartjs-plugin-datalabels';
-import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
-import { Color } from 'chartjs-plugin-datalabels/types/options';
+// import DatalabelsPlugin from 'chartjs-plugin-datalabels';
+// import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+// import { Color } from 'chartjs-plugin-datalabels/types/options';
 
 @Component({
   templateUrl: './dashboard.component.html',
@@ -34,36 +34,36 @@ export class DashboardComponent implements OnInit {
     })
   }
 
-  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  // @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
 
   // Pie
-  public pieChartOptions: ChartConfiguration['options'] = {
-    responsive: true,
-    plugins: {
-      legend: {
-        display: true,
-        position: 'right',
-      },
-      datalabels: {
-        formatter: (value, ctx) => {
-          if (ctx.chart.data.labels) {
-            return ctx.chart.data.labels[ctx.dataIndex];
-          }
-        },
-      },
-    }
-  };
-  public pieChartData: ChartData<'pie', number[], string | string[]> = {
-    labels: [
-      //  ['completed'], ['processing'], ['pending'],['recheck']
-      ],
-    datasets: [ {
-      data: [ 50, 280, 150, 20 ]
-    } ],
+  // public pieChartOptions: ChartConfiguration['options'] = {
+  //   responsive: true,
+  //   plugins: {
+  //     legend: {
+  //       display: true,
+  //       position: 'right',
+  //     },
+  //     datalabels: {
+  //       formatter: (value, ctx) => {
+  //         if (ctx.chart.data.labels) {
+  //           return ctx.chart.data.labels[ctx.dataIndex];
+  //         }
+  //       },
+  //     },
+  //   }
+  // };
+  // public pieChartData: ChartData<'pie', number[], string | string[]> = {
+  //   labels: [
+  //     //  ['completed'], ['processing'], ['pending'],['recheck']
+  //     ],
+  //   datasets: [ {
+  //     data: [ 50, 280, 150, 20 ]
+  //   } ],
 
-  };
-  public pieChartType: ChartType = 'pie';
-  public pieChartPlugins = [ DatalabelsPlugin ];
+  // };
+  // public pieChartType: ChartType = 'pie';
+  // public pieChartPlugins = [ DatalabelsPlugin ];
 
-  colors = ['red', 'green', 'blue', 'yellow'];
+  // colors = ['red', 'green', 'blue', 'yellow'];
 }
