48f965604d8b721f86fe1dcf30f1508f03b7f5dc
Umesh Thapa <Umesh.Thapa@veniosystems.com>
2023-07-01 19:45:56 +0545

    updade

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-   apiURL: 'http://127.0.0.1:8000',
+  //  apiURL: 'http://127.0.0.1:8000',
   // apiURL: 'http://64.227.182.105:8001'
-  //apiURL: 'https://limsserver.kantipurinfotech.com.np',
+  apiURL: 'https://limsserver.kantipurinfotech.com.np',
   socketUrl: 'ws://192.168.1.179:8000/asynchronous/chat'
 };
